43c7acc8e115d17a41abf31b3f9230d3d566e2d7
Matteus Varlesse <varlesse04@gmail.com>
2024-08-30 20:34:59 -0300

    fix: public route messages

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,16 +1,19 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { MessagesService } from './messages.service';
 import { CreateMessageDto } from './dto/create-message.dto';
+import { Public } from 'src/constant';
 
 @Controller('messages')
 export class MessagesController {
   constructor(private readonly messagesService: MessagesService) {}
 
+  @Public()
   @Post()
   create(@Body() createMessageDto: CreateMessageDto) {
     return this.messagesService.create(createMessageDto);
   }
 
+  @Public()
   @Get(':tripId')
   findAll(@Param('tripId') tripId: string) {
     return this.messagesService.findAll(tripId);
